6d1594e613a4884dd8fa22ea6759b67cee053f27
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-11-04 21:05:14 +0000

    fixed GET request

--- dating-backend/personas/views.py
+++ dating-backend/personas/views.py
@@ -16,7 +16,7 @@ def index(request):
 class PersonaListView(views.APIView):
     def get(self, request):
         personas = Persona.objects.all()
-        serialized_persona = PersonaSerializer(personas, request)
+        serialized_persona = PersonaSerializer(personas, many=True)
         return response.Response(serialized_persona.data, status=status.HTTP_200_OK)
 
     def post(self, request):
@@ -25,3 +25,4 @@ class PersonaListView(views.APIView):
         if persona_to_add.is_valid():
             persona_to_add.save()
             return response.Response(persona_to_add.data, status=status.HTTP_201_CREATED)
+        return response.Response(persona_to_add.errors, status=status.HTTP_400_BAD_REQUEST)
