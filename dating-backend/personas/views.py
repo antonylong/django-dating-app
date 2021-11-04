c801abe919d0eec1cdac6422d9c5a96a4b35a853
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-11-04 21:37:04 +0000

    add detailed views and urls checked in postman

--- dating-backend/personas/views.py
+++ dating-backend/personas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from rest_framework import views, response, status
+from rest_framework import views, response, status, exceptions
 
 # add serilaizer when done
 from .serializers import PersonaSerializer
@@ -14,7 +14,7 @@ def index(request):
 # check serializer name matches
 
 class PersonaListView(views.APIView):
-    def get(self, request):
+    def get(self, _request):
         personas = Persona.objects.all()
         serialized_persona = PersonaSerializer(personas, many=True)
         return response.Response(serialized_persona.data, status=status.HTTP_200_OK)
@@ -26,3 +26,28 @@ class PersonaListView(views.APIView):
             persona_to_add.save()
             return response.Response(persona_to_add.data, status=status.HTTP_201_CREATED)
         return response.Response(persona_to_add.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PersonaDetailView(views.APIView):
+    def get_persona_by_id(self, id):
+        try:
+            return Persona.objects.get(id=id)
+        except Persona.DoesNotExist:
+            raise exceptions.NotFound(detail="Profile does not exist")
+
+    def get(self, _request, id):
+        persona = self.get_persona_by_id(id)
+        serialized_persona = PersonaSerializer(persona)
+        return response.Response(serialized_persona.data, status=status.HTTP_200_OK)
+
+    def delete(self, _request, id):
+        persona = self.get_persona_by_id(id)
+        persona.delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, id):
+        persona = self.get_persona_by_id(id)
+        updated_persona = PersonaSerializer(persona, data=request.data)
+        if updated_persona.is_valid():
+            updated_persona.save()
+            return response.Response(updated_persona.data, status=status.HTTP_202_ACCEPTED)
+        return response.Response(updated_persona.errors, status=status.HTTP_400_BAD_REQUEST)
