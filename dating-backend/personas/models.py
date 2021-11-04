51b12fd025cb50c2a49751d7d66d8a8db2b11972
antony <dearalong@tutamail.com>
2021-11-04 18:39:06 +0000

    created models

--- dating-backend/personas/models.py
+++ dating-backend/personas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Persona(models.model):
+    name = models.CharField(max_length=50)
+    age = models.CharField(max_length=50)
+    height = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50)
+    sexuality = models.CharField(max_length=50)
+    occupation = models.CharField(max_length=50)
+    bio = models.CharField(max_length=500)
+    profile_pic = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name} ({self.id}"
