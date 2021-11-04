d2b54838a2ec56f6287ca83aedaaf874bbe963ff
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-11-04 20:36:19 +0000

    get and post persona view.py

--- dating-backend/personas/views.py
+++ dating-backend/personas/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 
+from rest_framework import views, response, status
+
+# add serilaizer when done
+from .serializers import PersonaSerializer
 from .models import Persona
-
-# Create your views here.
-
 
 def index(request):
     list = Persona.objects.all()
-
     context = {"personas": list}
-
     return render(request, "personas/index.html", context)
+
+# check serializer name matches
+
+class PersonaListView(views.APIView):
+    def get(self, request):
+        personas = Persona.objects.all()
+        serialized_persona = PersonaSerializer(personas, request)
+        return response.Response(serialized_persona.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        print(request.data)
+        persona_to_add = PersonaSerializer(data=request.data)
+        if persona_to_add.is_valid():
+            persona_to_add.save()
+            return response.Response(persona_to_add.data, status=status.HTTP_201_CREATED)
