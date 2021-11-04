662504a46663f75149b5b7cfaa41599c0439fd76
antony <dearalong@tutamail.com>
2021-11-04 18:38:42 +0000

    initial personas component

--- dating-backend/personas/apps.py
+++ dating-backend/personas/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UsersConfig(AppConfig):
+class PersonasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Users'
+    name = 'personas'
