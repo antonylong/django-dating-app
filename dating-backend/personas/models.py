ebdfbfc303956bb298744d57223b80a49c3ad6f9
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-11-06 15:47:19 +0000

    one to one users and personas

--- dating-backend/personas/models.py
+++ dating-backend/personas/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Persona(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     age = models.CharField(max_length=3)
     height = models.CharField(max_length=50)
