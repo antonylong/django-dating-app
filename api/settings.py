9cff26820d1357607a12406f675ee300317b9d76
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-11-20 17:02:44 +0000

    cors fix

--- api/settings.py
+++ api/settings.py
@@ -83,7 +83,6 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     'http://127.0.0.1:8000',
     "https://ani-mate-django.herokuapp.com",
-    "https://ani-mate-django.herokuapp.com/"
 ]
 
 
