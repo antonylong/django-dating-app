fd2d8d0d463906b40b45226e4645cf284ab6c375
Antony <84878245+antonylong@users.noreply.github.com>
2021-11-20 14:54:21 +0000

    semi-working stage

--- dating-backend/personas/models.py
+++ dating-backend/personas/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
+from django.contrib import admin
 
 #def upload_to(instance, filename):
 #    return 'posts/{filename}'.format(filename=filename)
 
+class Comment(models.Model):
+    profile = models.ForeignKey(Persona, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=400)
+    body = models.TextField()
+    dated_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "%s - %s" % (self.post.title, self.name)
 
 class Persona(models.Model):
     user = models.OneToOneField(
@@ -47,9 +56,23 @@ class Persona(models.Model):
         max_length=9, choices=SEXUALITY_CHOICE, default="Other")
 
     occupation = models.CharField(max_length=50)
-    bio = models.CharField(max_length=500)
+    bio = models.TextField()
     #profile_pic = models.ImageField(_("Image"), upload_to=upload_to, default='posts/default.png', null=True, blank=True)
     profile_pic = models.CharField(max_length=500, blank=True)
 
+    comment = models.ForeignKey(Comment, )
+
     def __str__(self):
         return f"{self.name} ({self.id})"
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+# now we can use persona.comments.all() to get all the comments to a persona??
--- dating-backend/personas/serializers.py
+++ dating-backend/personas/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Persona
+from .models import Comment, Persona
 
 
 class PersonaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Persona
+        fields = "__all__"
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
         fields = "__all__"
